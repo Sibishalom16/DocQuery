2e5b7db7bf64886c5e6515ecfb493bc35e0e3342
Sibi Shalom <shalomsibi16@gmail.com>
2026-09-02 15:41:47 +0530

    Add user registration API

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from database.connection import SessionLocal
+from database.models import User
+from backend.schemas import UserRegister
+from backend.security import hash_password
 
 app = FastAPI(title="DocQuery API")
 
@@ -17,3 +20,32 @@ def get_db():
 @app.get("/")
 def root():
     return {"message": "DocQuery API is running"}
+
+
+@app.post("/register")
+def register(user_data: UserRegister, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(
+        User.email == user_data.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    new_user = User(
+        name=user_data.name,
+        email=user_data.email,
+        password_hash=hash_password(user_data.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User registered successfully",
+        "user_id": new_user.id
+    }
